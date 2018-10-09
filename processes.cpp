ce2b2c3676128c42fd6e15bac0cad90ad44ec442
emma <ecglass@uw.edu>
2018-10-08 23:56:34 -0700

    Added in piping logic between parent/children according to my design

--- processes.cpp
+++ processes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 
@@ -10,15 +11,23 @@ using namespace std;
 int main(int argc, char * argv[]) {
     pid_t pid; //process id
 
-    int filedescriptor[2]; // file descriptors created
+    int filedescriptor1[2]; // 2 file descriptors created
+    int filedescriptor2[2]; // since the command has 2 pipes
     int status;
     int read = 0; // reading data from a pipe index
     int write = 1; //writing data to a pipe index
 
 
-    if (pipe(filedescriptor) < 0) { //Pipe created
+    if (pipe(filedescriptor1) < 0) { //Pipe fail
         perror("pipe error");
+        return EXIT_FAILURE;
     }
+
+    if (pipe(filedescriptor2) < 0) { //Pipe fail
+        perror("pipe error");
+        return EXIT_FAILURE;
+    }
+
 
     if ((pid = fork()) < 0) {
         perror("fork error");
@@ -37,20 +46,34 @@ int main(int argc, char * argv[]) {
                 return EXIT_FAILURE;
 
             } else if (pid == 0) { //great grand child
+                close(filedescriptor2[write]); //does not touch 2nd pipe
+                close(filedescriptor2[read]); //does not touch 2nd pipe
+                close(filedescriptor1[read]); //does not read anything.
                 execlp("ps", "ps", "-A", NULL);
 
             } else { //grandchild else
+
+                close(filedescriptor1[write]); //does not write to grandchild. -> reads from granchild
+
+                close(filedescriptor2[read]); //writes to 2nd pipe, so does not read
 
                 execlp("grep", "grep", argv[1], NULL);
 
             }
 
         } else { //child else
-
-            execlp("wc", "wc", "-l", NULL); // pipe back to parent
+            cout << "child";
+            close(filedescriptor2[write]); //does not write to pipe
+            close(filedescriptor1[read]);  //does not directly touch 1st pipe
+            close(filedescriptor1[write]); //does not directly touch 1st pipe
+            execlp("wc", "wc", "-l", NULL);
         }
     } else { //parent else
-        wait(&status);
+
+        wait(NULL);
+        cout << "parent";
+        exit(0);
+
 
     }
     return 0;
